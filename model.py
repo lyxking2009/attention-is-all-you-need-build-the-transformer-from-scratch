d88592c7cee6aafaac4f9c8fecb5eebe03c6b598
Yuxing Lin <lyxking2009@gmail.com>
2026-08-16 14:24:58 -0700

    Step 3: encode_sentence_to_ids

--- model.py
+++ model.py
@@ -23,8 +23,9 @@ def build_id_to_token_vocab(token_to_id):
     # TODO: build the inverse id-to-token dictionary from token_to_id
     return {token_id: token for token, token_id in token_to_id.items()}
 
-# Step 3 - encode_sentence_to_ids (not yet solved)
-# TODO: implement
+# Step 3 - encode_sentence_to_ids
+def encode_sentence_to_ids(sentence, token_to_id, unk_token='<unk>'):
+    return [token_to_id.get(token, token_to_id[unk_token]) for token in sentence.split()]
 
 # Step 4 - decode_ids_to_tokens (not yet solved)
 # TODO: implement
