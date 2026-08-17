c8489d57124b1915326bdfe1c1f318c1fbec42ab
Yuxing Lin <lyxking2009@gmail.com>
2026-08-17 12:44:59 -0700

    Step 41: assemble_encoder_layer

--- model.py
+++ model.py
@@ -212,8 +212,10 @@ def encoder_layer_feed_forward_sublayer(x, w1, b1, w2, b2, gamma, beta):
     ffn_output = position_wise_feed_forward_network(x, w1, b1, w2, b2)
     return apply_residual_add_and_norm(x, ffn_output, gamma, beta)
 
-# Step 41 - assemble_encoder_layer (not yet solved)
-# TODO: implement
+# Step 41 - assemble_encoder_layer
+def assemble_encoder_layer(x, layer_params, num_heads, src_mask=None):
+    x = encoder_layer_self_attention_sublayer(x, layer_params['w_q'], layer_params['w_k'], layer_params['w_v'], layer_params['w_o'], layer_params['attn_gamma'], layer_params['attn_beta'], num_heads, src_mask)
+    return encoder_layer_feed_forward_sublayer(x, layer_params['w1'], layer_params['b1'], layer_params['w2'], layer_params['b2'], layer_params['ffn_gamma'], layer_params['ffn_beta'])
 
 # Step 42 - stack_encoder_layers (not yet solved)
 # TODO: implement
