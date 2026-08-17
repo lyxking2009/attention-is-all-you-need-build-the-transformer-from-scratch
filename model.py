4e6362aca3142dbea4939cf147972d4bff5f8f21
Yuxing Lin <lyxking2009@gmail.com>
2026-08-17 09:45:24 -0700

    Step 19: mask_attention_scores_with_neg_inf

--- model.py
+++ model.py
@@ -100,8 +100,9 @@ def compute_raw_attention_scores(query, key):
 def scale_attention_scores(scores, d_k):
         return scores / (d_k ** 0.5)
 
-# Step 19 - mask_attention_scores_with_neg_inf (not yet solved)
-# TODO: implement
+# Step 19 - mask_attention_scores_with_neg_inf
+def mask_attention_scores_with_neg_inf(scores, mask):
+        return scores.masked_fill(~mask, float("-inf"))
 
 # Step 20 - softmax_attention_weights (not yet solved)
 # TODO: implement
