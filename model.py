c73ac05093a11c829ae8189cbcf702a1d849de95
Yuxing Lin <lyxking2009@gmail.com>
2026-08-17 14:36:36 -0700

    Step 75: compute_length_penalty

--- model.py
+++ model.py
@@ -478,8 +478,9 @@ def run_training_loop_for_steps(batches, parameter_list, model_params, optimizer
 def pick_next_token_by_argmax(final_timestep_logits):
     return final_timestep_logits.argmax(dim=-1)
 
-# Step 75 - compute_length_penalty (not yet solved)
-# TODO: implement
+# Step 75 - compute_length_penalty
+def compute_length_penalty(length, alpha):
+    return float(((5 + length) / 6) ** alpha)
 
 # Step 76 - compute_candidate_scores (not yet solved)
 # TODO: implement
