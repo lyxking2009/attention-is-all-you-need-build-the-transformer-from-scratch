47209ee46b79c281217e6c0225f6f29de3168265
Yuxing Lin <lyxking2009@gmail.com>
2026-08-17 13:06:56 -0700

    Step 50: apply_log_softmax_over_vocab

--- model.py
+++ model.py
@@ -255,8 +255,9 @@ def apply_final_output_projection(decoder_output, output_weight, output_bias=Non
 def tie_output_projection_to_token_embeddings(token_embedding_weight):
     return token_embedding_weight.transpose(0, 1)
 
-# Step 50 - apply_log_softmax_over_vocab (not yet solved)
-# TODO: implement
+# Step 50 - apply_log_softmax_over_vocab
+def apply_log_softmax_over_vocab(logits):
+    return torch.log_softmax(logits, dim=-1)
 
 # Step 51 - run_transformer_forward (not yet solved)
 # TODO: implement
